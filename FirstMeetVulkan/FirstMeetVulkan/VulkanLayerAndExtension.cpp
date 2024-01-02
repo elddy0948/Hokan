7b1119b2583f39606ba46f8fd59ca447c215a50e
Joons <elddy0948@gmail.com>
2024-01-02 10:27:43 +0900

    get layers and extensions

--- FirstMeetVulkan/FirstMeetVulkan/VulkanLayerAndExtension.cpp
+++ FirstMeetVulkan/FirstMeetVulkan/VulkanLayerAndExtension.cpp
@@ -18,10 +18,10 @@ VkResult VulkanLayerAndExtension::getInstanceLayerProperties() {
 	} while (result == VK_INCOMPLETE);
 
 	std::cout << "\nInstanced Layers\n";
-	std::cout << "====================\n";
+	std::cout << "============================================================\n";
 
 	for (const auto& globalLayerProperty : layerProperties) {
-		std::cout << '\n' << globalLayerProperty.description << "\n\t|\t-[Layer Name]-> " << globalLayerProperty.layerName << '\n';
+		std::cout << '\n' << globalLayerProperty.description << "\n\t|\n\t|---[Layer Name]-> " << globalLayerProperty.layerName << '\n';
 
 		LayerProperties lp;
 		lp.properties = globalLayerProperty;
@@ -42,9 +42,33 @@ VkResult VulkanLayerAndExtension::getInstanceLayerProperties() {
 }
 
 VkResult VulkanLayerAndExtension::getExtensionProperties(LayerProperties& layerProperties, VkPhysicalDevice* physicalDevice) {
-	
+	uint32_t extensionCount;
+	std::vector<VkExtensionProperties> extensions;
+	VkResult result;
+
+	do {
+		if (physicalDevice)
+			result = vkEnumerateDeviceExtensionProperties(*physicalDevice, layerProperties.properties.layerName, &extensionCount, nullptr);
+		else
+			result = vkEnumerateInstanceExtensionProperties(layerProperties.properties.layerName, &extensionCount, nullptr);
+
+
+		if (result) { return result; }
+		if (extensionCount == 0) { return VK_INCOMPLETE; }
+
+		extensions.resize(extensionCount);
+
+		if (physicalDevice)
+			result = vkEnumerateDeviceExtensionProperties(*physicalDevice, layerProperties.properties.layerName, &extensionCount, extensions.data());
+		else
+			result = vkEnumerateInstanceExtensionProperties(layerProperties.properties.layerName, &extensionCount, extensions.data());
+	} while (result == VK_INCOMPLETE);
+
+	layerProperties.extensions = extensions;
+
+	return result;
 }
 
 VkResult VulkanLayerAndExtension::getDeviceExtensionProperties(VkPhysicalDevice* physicalDevice) {
-
+	return VK_INCOMPLETE;
 }
--- FirstMeetVulkan/FirstMeetVulkan/VulkanLayerAndExtension.h
+++ FirstMeetVulkan/FirstMeetVulkan/VulkanLayerAndExtension.h
@@ -10,13 +10,18 @@ struct LayerProperties {
 };
 
 class VulkanLayerAndExtension {
-private:
+public:
 	VkResult getInstanceLayerProperties();
 	VkResult getExtensionProperties(LayerProperties& layerProperties, VkPhysicalDevice* physicalDevice = NULL);
 	VkResult getDeviceExtensionProperties(VkPhysicalDevice* physicalDevice);
 
+	void setInstanceLayerNames(std::vector<const char*> names) { instanceLayerNames = names; }
+	void setInstanceExtensionNames(std::vector<const char*> names) { instanceExtensionNames = names; }
+
 private:
 	std::vector<LayerProperties> layerPropertyList;
+	std::vector<const char*> instanceLayerNames;
+	std::vector<const char*> instanceExtensionNames;
 };
 
 #endif /* !VULKAN_LAYER_AND_EXTENSION_H */

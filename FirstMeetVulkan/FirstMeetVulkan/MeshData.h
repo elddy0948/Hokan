9ce62fc80bf927c9d59903861cbdcdf58cf81bbb
Joons <elddy0948@gmail.com>
2024-01-09 10:56:33 +0900

    feat: MeshData

---
+++ FirstMeetVulkan/FirstMeetVulkan/MeshData.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef MESH_DATA_H
+#define MESH_DATA_H
+
+struct VertexWithColor {
+	float x, y, z, w;
+	float r, g, b, a;
+};
+
+static const VertexWithColor triangleData[] = {
+	{0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
+	{1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f},
+	{-1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f},
+};
+
+#endif /* !MESH_DATA_H */
